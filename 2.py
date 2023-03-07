055e6994052706d2965cefe9f12b5c3522efe38b
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2023-03-07 17:02:06 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+import datetime
+
+month, year = map(int, input().split())
+print('пн\tвт\tср\tчт\tпт\tсб\tвс\t')
+date = datetime.date(year, month, 1)
+delta = datetime.timedelta(days=1)
+week = ['' for _ in range(7)]
+while date.month == month:
+    week[date.weekday()] = str(date.day)
+    if date.weekday() == 6:
+        print('\t'.join(week) + '\t')
+    date += delta
