caa31be8f378ce06ada1881a5f28089c81827b3b
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2023-03-07 17:20:13 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,4 +9,10 @@ while date.month == month:
     week[date.weekday()] = str(date.day)
     if date.weekday() == 6:
         print('\t'.join(week) + '\t')
+        week = ['' for _ in range(7)]
     date += delta
+for day in week:
+    if day:
+        print(day, end='\t')
+if week.count('') != 7:
+    print()
